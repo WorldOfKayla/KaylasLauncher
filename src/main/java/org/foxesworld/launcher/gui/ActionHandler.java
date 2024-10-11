9ecff40b0162c9a23a20f298c18eee0c93f0adbe
Aiden <miomoor182@gmail.com>
2024-10-11 20:09:42 +0300

    1.11.3-RENEGADE

--- src/main/java/org/foxesworld/launcher/gui/ActionHandler.java
+++ src/main/java/org/foxesworld/launcher/gui/ActionHandler.java
@@ -86,6 +86,7 @@ public class ActionHandler extends org.foxesworld.engine.gui.ActionHandler {
                         }
                     };
                     String sound = this.launcher.getSOUND().playSound("other", "ogo", listener);
+                    this.launcher.getLoadingManager().toggleLoader();
                     //this.launcher.getNotification().display("Sound Test", sound, this.launcher.getIconUtils().getVectorIcon("assets/ui/icons/aidenfox.svg", 128, 128));
                 }
 
